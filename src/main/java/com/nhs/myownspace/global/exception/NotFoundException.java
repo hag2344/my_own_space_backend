c78d17b692250bd0f840e9e34f5b05387921bb49
Haksu Nam <hag2344@naver.com>
2025-12-18 07:12:22 +0900

    add(all) : common exception add

---
+++ src/main/java/com/nhs/myownspace/global/exception/NotFoundException.java
@@ -0,0 +1,7 @@
+package com.nhs.myownspace.global.exception;
+
+public class NotFoundException extends RuntimeException {
+    public NotFoundException(String message) {
+        super(message);
+    }
+}
